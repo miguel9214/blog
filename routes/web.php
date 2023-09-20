86af2097ac0d0c8c78c59c472d5984ae23508f09
Miguel Ramos <miguel921433@gmail.com>
2023-09-19 22:25:01 -0500

    template realizada

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+get:consultar
+post:salvar
+delete:elminar
+put:actualizar
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    return view('home');
+})->name('home');
+
+Route::get('blog', function () {
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel'],
+    ];
+
+    return view('blog', ['posts' => $posts]);
+})->name('blog');
+
+
+Route::get('blog/{slug}', function ($slug) {
+
+    $post = $slug;
+
+    return view('post', ['post' => $post]);
+})->name('post');
