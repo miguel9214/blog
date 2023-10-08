17fec0c5b16b70f38ff35f5f646f39bcaa65cf7b
Miguel Ramos <miguel921433@gmail.com>
2023-10-08 09:14:34 -0500

    agregado resgister

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -28,9 +28,9 @@
             @auth
                 <a href="{{ route('dashboard') }}">Dashboard</a>
             @else
-                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('login') }}"  class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Login</a>
             @endauth
-
+            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
         </header>
 
         <div class="h-px mb-8 opacity-60"
